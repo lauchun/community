a1f9569f014452e2fe1a33be9cdc940d9bf4cac7
lauchun <747358601@qq.com>
2020-02-24 22:19:05 +0800

    add check cookies

--- src/main/java/com/lau/community/controller/IndexController.java
+++ src/main/java/com/lau/community/controller/IndexController.java
@@ -1,9 +1,15 @@
 package com.lau.community.controller;
 
+import com.lau.community.mapper.UserMapper;
+import com.lau.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author ：lauchun
@@ -14,8 +20,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user",user);
+                    }
+                    break;
+                }
+            }
+        }
+
         return "index";
     }
 }
